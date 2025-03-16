74c82fc4efa7aed3f901279a3f03aa7bbf68294f
MatheusMalaquias <mateusotavio614@gmail.com>
2025-03-16 00:18:51 -0300

    Updated

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Journal
+{
+    static void Main(string[] ars)
+    {
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("Please select one of the following choice.");
+        Console.WriteLine("1");
+        Console.WriteLine("2");
+        Console.WriteLine("3");
+        Console.WriteLine("4");
+        Console.WriteLine("5");
+
+        Console.Write("What would you like to do? ");
+        string prompt = Console.ReadLine();
+    }
+}
